004ba0a536514fd136c47e950399d72bf9137f83
Han yunshik <ojokdol@daum.net>
2025-03-11 17:11:10 +0900

    env설정

--- app/api/pwrite/route.ts
+++ app/api/pwrite/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8001";
+
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
     console.log("전송 데이터:", formData);
 
     const res = await axios.post(
-      "http://arumcnc7.cafe24app.com/api/pwrite",
+      `${API_BASE_URL}/api/pwrite`, // 환경 변수 사용
       formData,
       { headers: { "Content-Type": "multipart/form-data" } }
     );
