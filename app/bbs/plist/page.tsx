84cb66c95f23b4597f1d8a894c796efcd5118614
Han yunshik <ojokdol@daum.net>
2025-03-11 15:44:27 +0900

    imgview

--- app/bbs/plist/page.tsx
+++ app/bbs/plist/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-
 export default function Plist() {
-  
+  const imageUrl = encodeURIComponent(
+    "http://arumcnc7.cafe24app.com/home/hosting_users/arumcnc7/apps/arumcnc7_arumcnc7/uploads/1741674653887-548438850.jpg"
+  );
+
   return (
     <div>
       <h1>Gallery List</h1>
-      <img src="http://arumcnc7.cafe24app.com/home/hosting_users/arumcnc7/apps/arumcnc7_arumcnc7/uploads/1741674653887-548438850.jpg" />
+      <img src={`/api/image-proxy?url=${imageUrl}`} alt="Gallery Image" />
     </div>
   );
 }
